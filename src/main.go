4413c99c58291cc1908aaa8849c44c46e84d4dc3
Zi Wang <ziwang@uber.com>
2016-04-30 16:46:40 -0700

    Initial commit with gulp support

---
+++ src/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/golang-distributed-application/src/test"
+
+func main() {
+	test.PrintDummy()
+}
---
+++ src/test/dummy.go
@@ -0,0 +1,8 @@
+package test
+
+import "fmt"
+
+// PrintDummy prints dummy message
+func PrintDummy() {
+	fmt.Println("Printing dummy...")
+}
